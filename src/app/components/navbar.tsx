f36ca8f1a38fa82937d91dbcfd4d9d4a9e34fe3a
Kevin <kevinsl192k@gmail.com>
2024-02-03 00:30:11 -0800

    will continue at work

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -1,4 +1,5 @@
 "use client";
+import classNames from "classnames";
 import { useState } from "react";
 import { motion } from "framer-motion";
 
@@ -12,43 +13,44 @@ export default function NavBar() {
   type NavButtonProps = {
     text?: string;
     onHover?: () => void;
+    colStart?: number;
+    rowStart?: number;
   };
 
-  function NavButton({ text, onHover }: NavButtonProps) {
+  function NavButton({ text, onHover, colStart, rowStart}: NavButtonProps) {
     return (
-      <button
-        className="text-xs w-12 w-12 text-slate-700 mx-10 hover:text-sky-50"
+      <div className={`row-start-${rowStart} col-start-${colStart}`}>
+        <button
+        className="flex justify-center items-center text-sm w-fit text-slate-700 hover:text-sky-50"
         onMouseEnter={onHover}
       >
         {text}
       </button>
+      </div>
     );
   }
 
   function Dropdown() {
     return (
       isDropdownVisible && (
-        <motion.div
+        <div className={`grid grid-cols-3 h-auto w-full ${
+            isDropdownVisible ? "bg-sky-600" : "bg-sky-600/75"
+          }`}>
+          <motion.div
           initial={{ scaleY: 0 }}
           animate={{ scaleY: 1 }}
           style={{ transformOrigin: "top" }}
-          className={`h-auto flex flex-col w-screen ${
-            isDropdownVisible ? "bg-sky-600" : "bg-sky-600/75"
-          }`}
-        >
-          <div className={`flex min-h-10 items-center justify-center`}>
-            <NavButton text="Method" />
-            <NavButton text="Analysis" />
-            <NavButton />
-            <NavButton />
-          </div>
-          <div className={`flex min-h-10 items-center justify-center`}>
-            <NavButton text="Story" />
-            <NavButton text="" />
-            <NavButton />
-            <NavButton />
-          </div>
-        </motion.div>
+          className={`grid col-start-2 col-end-3 grid-cols-4 grid-rows-3`}
+          >
+            {/*Data*/}
+            <NavButton text="Analysis" colStart={1} rowStart={1}/>
+            <NavButton text="Lorem" colStart={1} rowStart={3}/>
+            {/*Narrative*/}
+            <NavButton text="Story" colStart={2} rowStart={1}/>
+            <NavButton text="Report"/>
+            {/*Data*/}
+          </motion.div>
+        </div>
       )
     );
   }
@@ -56,18 +58,18 @@ export default function NavBar() {
   return (
     <div
       onMouseLeave={handleMouseLeave}
-      className={`fixed flex flex-col
+      className={`fixed flex flex-col w-screen
       }`}
     >
       <div
-        className={`grid grid-cols-3 max-w-screen min-h-10 ${
+        className={`grid grid-cols-3 h-10 ${
           isDropdownVisible ? "bg-sky-600" : "bg-sky-600/75"
         }`}
       >
-        <h1 className="flex items-center col-start-1 ml-10 col-end-2 text-lg">
+        <h1 className="flex items-center col-start-1 col-end-2 pl-10 font-bold text-xl">
           RIKVA
         </h1>
-        <div className={`flex items-center col-start-2 col-end-3`}>
+        <div className={`flex justify-around items-center col-start-2 col-end-3`}>
           <NavButton text="Data" onHover={() => setDropdownVisible(true)} />
           <NavButton
             text="Narrative"
