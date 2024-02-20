7968c485ffe256f6b9c64cd38f82200070a82900
Kevin Li <kevinsl192k@gmail.com>
2024-02-20 15:44:59 -0800

    fixing deploy issue

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,26 +3,26 @@ import "./globals.css";
 import NavBar from "./components/navbar";
 import { Inter, DM_Sans, Roboto_Mono, Comfortaa } from "next/font/google";
 
-export const inter = Inter({
+const inter = Inter({
   subsets: ["latin"],
   display: "swap",
   variable: "--font-inter",
 });
 
-export const comfortaa = Comfortaa({
+const comfortaa = Comfortaa({
   subsets: ["latin"],
   display: "swap",
   variable: "--font-comfortaa",
 });
 
-export const roboto_mono = Roboto_Mono({
+const roboto_mono = Roboto_Mono({
   subsets: ["latin"],
   display: "swap",
   weight: "400",
   variable: "--font-roboto_mono",
 });
 
-export const dm_sans = DM_Sans({
+const dm_sans = DM_Sans({
   subsets: ["latin"],
   display: "swap",
   variable: "--font-dm_sans",
