94847eac7f36fec25e22d3529b544947e9b96994
Kevin Li <kevinsl192k@gmail.com>
2024-03-22 12:33:40 -0700

    trying2

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -8,7 +8,7 @@ import { motion } from "framer-motion";
 export default function About() {
   return (
     <div className="flex flex-col h-auto w-screen items-center max-w-screen min-h-screen">
-      <div className="bg-flight-pattern bg-bottom bg-fixed bg-cover w-screen">
+      <div className="bg-flight-pattern bg-bottom bg-fixed bg-cover h-screen w-screen">
         <motion.div
           initial={{ scaleX: 1 }}
           animate={{
